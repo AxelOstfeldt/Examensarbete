48eebd18ef46cfc645706b1aafef532318717211
axos8381 <axel.ostfeldt.8381@student.uu.se>
2024-02-01 21:50:56 +0100

    Changed to a class

--- Golomb.py
+++ Golomb.py
@@ -1,85 +1,92 @@
 import math
 import numpy as np
 
+class GolombCoding:
+
+    def __init__(self, m, sign):
+        self.m = m
+        self.sign = sign
+
+    def Golomb_encode(self, n):
 
 
-def Golomb_encode(n, m, sign):
+        q = math.floor(n/ self.m)
+        r = n % self.m
+        cc = math.log(self.m,2)
+        c = math.ceil(cc)
+        b = pow(2,c) - self.m
 
+        encoded = bin(0)[2:]
 
-    q = math.floor(n/m)
-    r = n % m
-    c = math.ceil(math.log(m,2))
-    b = pow(2,c) - m
+        if self.sign == True:
+            if n < 0:
+                s = "1"
+                n = -n
+            else:
+                s = "0"
+                
 
-    encoded = bin(0)[2:]
+        for i in range(q):
+            encoded = "1" + encoded
 
-    if sign == True:
-        if n < 0:
-            s = "1"
-            n = -n
+        if cc.is_integer():
+            encoded = encoded + np.binary_repr(r, width = c)
+        elif r < b:
+            encoded = encoded + np.binary_repr(r, width = c-1)
         else:
-            s = "0"
-            
+            encoded = encoded + np.binary_repr(r+b, width = c)
 
-    for i in range(q):
-        encoded = "1" + encoded
+        if self.sign == True:
+            encoded = s + encoded
 
-    if (math.log(m,2)).is_integer():
-        encoded = encoded + np.binary_repr(r, width = c)
-    elif r < b:
-        encoded = encoded + np.binary_repr(r, width = c-1)
-    else:
-        encoded = encoded + np.binary_repr(r+b, width = c)
-
-    if sign == True:
-        encoded = s + encoded
-
-    return encoded
+        return encoded
 
 
 
-def Golomb_decode(code, m, sign):
+    def Golomb_decode(self, code):
 
-    c = math.ceil(math.log(m,2))
-    b = pow(2,c) - m
-    A = 0
+        cc = math.log(self.m,2)
+        c = math.ceil(cc)
+        b = pow(2,c) - self.m
+        A = 0
 
-    if sign == True:
-        if code[0] == "1":
-            ss = "1"
+        if self.sign == True:
+            if code[0] == "1":
+                ss = "1"
+            code = code[1:]
+
+        while code[0] == "1":
+            A += 1
+            code = code[1:]
         code = code[1:]
 
-    while code[0] == "1":
-        A += 1
-        code = code[1:]
-    code = code[1:]
+        
 
-    
-
-    if (math.log(m,2)).is_integer():
-        decoded = m * A + int(encoded, 2)
-    else:
-        R = int(code[:c-1],2)
-        if R < b:
-            decoded = m * A + int(code, 2)
+        if (cc).is_integer():
+            decoded = self.m * A + int(code, 2)
         else:
-            decoded = m * A + int(code, 2) - b
+            R = int(code[:c-1],2)
+            if R < b:
+                decoded = self.m * A + int(code, 2)
+            else:
+                decoded = self.m * A + int(code, 2) - b
 
 
 
 
-    if sign == True and ss == "1":
-        decoded = -decoded
+        if self.sign == True and ss == "1":
+            decoded = -decoded
 
-    return decoded
+        return decoded
 
 
+Golomb_coder = GolombCoding(7, False)
 
-kodOrd = Golomb_encode(5, 7, False)
+kodOrd = Golomb_coder.Golomb_encode(5)
 
 print("kod ord: ",kodOrd)
 
-value = Golomb_decode(kodOrd, 7, False)
+value = Golomb_coder.Golomb_decode(kodOrd)
 
 print("Value: ", value)
         
