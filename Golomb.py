99cb2543577e05cae267fae6d5102fbf12aadce1
axos8381 <axel.ostfeldt.8381@student.uu.se>
2024-02-01 21:23:29 +0100

    Golomb uppdate

--- Golomb.py
+++ Golomb.py
@@ -1,15 +1,14 @@
 import math
 import numpy as np
 
-n = 5
-m = 7
+n = 7
+m = 11
 
 print("n: ", n)
 q = math.floor(n/m)
 r = n % m
 c = math.ceil(math.log(m,2))
 
-print(c)
 
 
 b = pow(2,c) - m
@@ -19,7 +18,7 @@ encoded = bin(0)[2:]
 
 sign = False
 
-if sign == True:
+if sign:
     if n < 0:
         s = "1"
         n = -n
@@ -29,22 +28,27 @@ if sign == True:
 
 for i in range(q):
     encoded = "1" + encoded
+    print("for loop: ", encoded)
 
 
 if (math.log(m,2)).is_integer():
     encoded = encoded + np.binary_repr(r, width = c)
-elif r >= pow(2,c) -m:
-    encoded = encoded + np.binary_repr(r+pow(2,c)-m, width = c)
-else:
+elif r < b:
     encoded = encoded + np.binary_repr(r, width = c-1)
+else:
+    encoded = encoded + np.binary_repr(r+b, width = c)
+
+if sign == True:
+    encoded = s + encoded
+
 
 print("encoded: ", encoded)
 
-R= 7
 
-Q = 0
-B = pow(2,c) - m
-A = 0
+
+qq = 0
+bb = pow(2,c) - m
+aa = 0
 decoded = 0
 #c = math.ceil(math.log(m,2)) for the function later
 #m = 7 for the function later
@@ -53,24 +57,37 @@ sign = False
 
 if sign == True:
     if encoded[0] == "1":
-        S = "1"
+        ss = "1"
     encoded = encoded[1:]
 
 while encoded[0] == "1":
-    A += 1
+    aa += 1
     encoded = encoded[1:]
 
 encoded = encoded[1:]
 
+#if (math.log(m,2)).is_integer():
+#    decoded = m * aa + int(encoded, 2)
+#elif len(encoded) == c-1:
+#    decoded = m * aa + int(encoded, 2)
+#else:
+#    decoded = m * aa + int(encoded, 2) - bb
+
+R = int(encoded[:c-1],2)
+
 if (math.log(m,2)).is_integer():
-    decoded = m * A + int(encoded)
-
-elif len(encoded) == c:
-    decoded = m * A + int(encoded)
+    decoded = m * aa + int(encoded, 2)
+elif R < bb:
+    decoded = m * aa + int(encoded, 2)
 else:
-    decoded = m * A + int(encoded) - B
+    decoded = m * aa + int(encoded, 2) - bb
 
-print(A)
+
+
+
+if sign == True and ss == "1":
+    decoded = -decoded
+
 
 print("Decoded: ", decoded)
 
