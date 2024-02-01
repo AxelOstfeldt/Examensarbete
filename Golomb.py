b61522d4400d1b249fad70003a71e2b91fa7a1c3
axos8381 <axel.ostfeldt.8381@student.uu.se>
2024-02-01 20:47:28 +0100

    asd

--- Golomb.py
+++ Golomb.py
@@ -1,15 +1,19 @@
 import math
+import numpy as np
 
-m = 4
-n = 4
+n = 5
+m = 7
 
+print("n: ", n)
 q = math.floor(n/m)
 r = n % m
 c = math.ceil(math.log(m,2))
 
-print("r = ", r)
+print(c)
+
+
 b = pow(2,c) - m
-print("b = ",b)
+
 
 encoded = bin(0)[2:]
 
@@ -26,30 +30,50 @@ if sign == True:
 for i in range(q):
     encoded = "1" + encoded
 
-if (math.log(m,2)).is_integer() or r < pow(2,c) -m :
-    encoded = encoded + bin(r)[2:]
+
+if (math.log(m,2)).is_integer():
+    encoded = encoded + np.binary_repr(r, width = c)
+elif r >= pow(2,c) -m:
+    encoded = encoded + np.binary_repr(r+pow(2,c)-m, width = c)
 else:
-    encoded = encoded + bin(r + pow(2,c) - m)[2:]
-    print("hej")
+    encoded = encoded + np.binary_repr(r, width = c-1)
 
+print("encoded: ", encoded)
 
+R= 7
 
 Q = 0
 B = pow(2,c) - m
+A = 0
+decoded = 0
+#c = math.ceil(math.log(m,2)) for the function later
+#m = 7 for the function later
+
+sign = False
 
 if sign == True:
     if encoded[0] == "1":
         S = "1"
-    else:
-        S = "0"
     encoded = encoded[1:]
 
-if (math.log(m,2)).is_integer() or R < pow(2,c) -m :
-    print("hej")
+while encoded[0] == "1":
+    A += 1
+    encoded = encoded[1:]
 
+encoded = encoded[1:]
 
+if (math.log(m,2)).is_integer():
+    decoded = m * A + int(encoded)
+
+elif len(encoded) == c:
+    decoded = m * A + int(encoded)
 else:
-    encoded = encoded + bin(r + pow(2,c) - m)[2:]
+    decoded = m * A + int(encoded) - B
+
+print(A)
+
+print("Decoded: ", decoded)
+
     
     
 
