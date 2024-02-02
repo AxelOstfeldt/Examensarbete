cfa024206e88c8d054d94513c57a88698e41bbe2
axos8381 <axel.ostfeldt.8381@student.uu.se>
2024-02-02 11:40:30 +0100

    some bugs in Golomb not fixed

--- Golomb.py
+++ Golomb.py
@@ -30,12 +30,17 @@ class GolombCoding:
         for i in range(q):
             encoded = "1" + encoded
 
+        if len(encoded) != q+1:
+            raise ValueError(f"Unary code size does not match q value. Current values are: encoded = {encoded}, q = {q}")
+
         if cc.is_integer():
             encoded = encoded + np.binary_repr(r, width = c)
         elif r < b:
             encoded = encoded + np.binary_repr(r, width = c-1)
         else:
             encoded = encoded + np.binary_repr(r+b, width = c)
+
+        
 
         if self.sign == True:
             encoded = s + encoded
@@ -51,6 +56,8 @@ class GolombCoding:
         b = pow(2,c) - self.m
         A = 0
         S = "0"
+        LengthCheck = len(code)
+        
 
         if self.sign == True:
             if code[0] == "1":
@@ -65,19 +72,38 @@ class GolombCoding:
         
 
         if (cc).is_integer():
+            if len(code) != c:
+                raise ValueError(f"Size error, length of code remaining should match c. Current values are: code = {code}, c = {c}")
             decoded = self.m * A + int(code, 2)
+            print("Case 1")
         else:
             R = int(code[:c-1],2)
             if R < b:
+                if len(code) != c-1:
+                    raise ValueError(f"Size error. code = {code}, should have length: {c-1}")
                 decoded = self.m * A + int(code, 2)
+                print("Case 2")
             else:
+                if len(code) != c:
+                    raise ValueError(f"Size error. code = {code}, should have length: {c}")
                 decoded = self.m * A + int(code, 2) - b
+                print("Case 3")
 
-
+        
 
 
         if self.sign and S == "1":
             decoded = -decoded
+            if ((cc).is_integer() or R >= b) and LengthCheck != A+c+2:
+                raise ValueError(f"Size error. Input code length, {LengthCheck}, should be equal to A+c+2 = {A+c+2}")
+            elif LengthCheck != A+c+1:
+                raise ValueError(f"Size error. Input code length, {LengthCheck}, should be equal to A+c+2 = {A+c+1}")
+        else:
+            if ((cc).is_integer() or R >= b) and LengthCheck != A+c+1:
+                raise ValueError(f"Size error. Input code length, {LengthCheck}, should be equal to A+c+2 = {A+c+1}")
+            elif LengthCheck != A+c:
+                raise ValueError(f"Size error. Input code length, {LengthCheck}, should be equal to A+c+2 = {A+c}")
+
 
         return decoded
 
