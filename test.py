08954aae03db4e106b15900bac6d307a2b0d8b63
AxelOstfeldt <axel.ostfeldt@gmail.com>
2024-02-07 10:12:06 +0100

    asd

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-n = [0,1,2,3,4,5]
-k = 4
-m = n[len(n)-k:]
-s = n[:len(n)-k]
-print(n[1])
+import numpy as np
+
+x = [1,2,3,4,5]
+
+def autocorrelation(x, lag):
+    x_mean = np.mean(x)
+    n = 0
+    t = 0
+    for i in range(lag, len(x)):
+        t += (x[t] - x_mean) * (x[t-lag] - x_mean)
+    for i in range(len(x)):
+        n += pow(x[i] - x_mean, 2)
