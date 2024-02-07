612946318394409f3293a32ad891e75388b24609
AxelOstfeldt <axel.ostfeldt@gmail.com>
2024-02-07 13:16:46 +0100

    asd

--- LPC.py
+++ LPC.py
@@ -31,37 +31,43 @@ for t in range(len(residuals)):
     R.append(autocorrelation(residuals,t))
 
 
-
+print(R)
 
 order = 5
 
-a = [0] * order
+a = [0]
 
-k = [0] * order
+k = [0]
 
-E = [0] * order
+n = order+1
 
-n = order
-
-E[0] = R[0]
+E = [R[0]]
 
 for i in range(1, n):
 
 
     a_old = a
+    k_temp = 0
+
+
 
     for j in range(i-1):
-        k[i] = a[j] * R[i-j] + k[i]
+        k_temp = a[j] * R[i-j] + k_temp
 
-    k[i] = ( R[i] - k[i] ) / E[i-1]
+    k_temp = ( R[i] - k_temp ) / E[i-1]
 
-    a[i] = k[i]
 
-    if i > 0:
-        for j in range (1, i-1):
+    k.append(k_temp)
+
+    a.append(k[i])
+    #print("i = ",i)
+    if i > 1:
+        for j in range (1, i):
+            #print("j = ", j)
             a[j] = a_old[j] - k[i]*a_old[i-j]
         
-    E[i] = (1 - pow(k[i],2) ) * E[i-1]
+        
+    E.append((1 - pow(k[i],2)) * E[i-1])
     
     #print("loop #i: ", i)
     #print("k = ", k)
@@ -70,19 +76,24 @@ for i in range(1, n):
     
 
 
-prediction = [0] * (len(residuals)-4)
+prediction = [0] * (len(residuals))
+diff = [0] * len(residuals)
 
-for i in range(len(residuals)-4):
-    print(i)
+#for i in range(4, len(residuals)):
+
     
     
-    #pred += 
+    #pred = a[1] * residuals[i-1] + a[2] * residuals[i-2] + a[3] * residuals[i-3] + a[4] * residuals[i-4]
 
-    prediction[i] = int(pred)
+    #prediction[i] = int(pred)
+
+for i in range(len(residuals)):
+    diff[i] = residuals[i] - prediction[i]
 
 
-print("Original values: ", residuals)
-print("Predicted values: ", prediction)
+#print("Original values: ", residuals)
+#print("Predicted values: ", prediction)
+#print("Residuals: ", diff)
         
 
 
