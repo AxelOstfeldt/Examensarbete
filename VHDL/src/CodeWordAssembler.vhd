adea4430b2a0f71e84f09cbc5908d5dfa74f47e7
AxelOstfeldt <115161796+AxelOstfeldt@users.noreply.github.com>
2024-04-15 14:29:22 +0200

    Uppdated file

--- VHDL/src/CodeWordAssembler.vhd
+++ VHDL/src/CodeWordAssembler.vhd
@@ -7,18 +7,18 @@ use ieee.numeric_std.all;
 entity CodeWordAssembler is
 
    port (
-      reset          : in std_logic;
-      clk            : in std_logic;
-      CodeWord       : in std_logic_vector(1023 downto 0);--CodeWord
-      CodeWordLength : in std_logic_vector(9 downto 0);
-      k_value        : in std_logic_vector(4 downto 0);--Saved as metadata
-      NewCodeWordIn  : in std_logic;--New codeword available for assemble
+      reset              : in std_logic;
+      clk                : in std_logic;
+      CodeWordIn         : in std_logic_vector(1023 downto 0);--CodeWord
+      CodeWordLenIn      : in std_logic_vector(9 downto 0);
+      k_valueIn          : in std_logic_vector(4 downto 0);--Saved as metadata
+      NewCodeWordReadyIn : in std_logic;--New codeword available for assemble
+      DataSavedIn        : std_logic;
 
-      AssembleDone       : out std_logic;--New CodeWord ready to be sent
-      ErrorOut           : out std_logic;--Send an error signal when error state is reached
-      ReadyToRecive      : out std_logic;--Indicates that it is ready to recive the next codeword to assemble
-      AllCodeWords       : out std_logic_vector(8191 downto 0);--All codewords together, length is 2^13 - 1
-      AllCodeWordsLength : out std_logic_vector(12 downto 0)--Count the total length of used bits in "AllCodeWords"
+      AllCodeWordsOut          : out std_logic_vector(8191 downto 0);--All codewords together, length is 2^13 - 1
+      AssembleDoneOut          : out std_logic;--New CodeWord ready to be sent
+      ReadyToReciveCodeWordOut : out std_logic;--Indicates that it is ready to recive the next codeword to assemble
+      ErrorOut                 : out std_logic--Send an error signal when error state is reached
 
    );
 
@@ -45,47 +45,44 @@ begin
             when Idle =>
                CheckState <= 0;
                --Reset all values
-               AllCodeWords       <= (others => '0');
-               AllCodeWordsLength <= (others => '0');
-               ErrorOut           <= '0';
-               AssembleDone       <= '0';
-               TotalLenCounter    <= 0;
-               CurrentCodeWordLen <= 0;
-               AssembleLenCounter <= 0;
-               CurrentCodeWord    <= (others => '0');
-               k_meta             <= (others => '0');
-               SampleCounter      <= 0;
-               SampleCounter_meta <= (others => '0');
-               ReadyToRecive      <= '1';
+               AllCodeWordsOut          <= (others => '0');
+               ErrorOut                 <= '0';
+               AssembleDoneOut          <= '0';
+               TotalLenCounter          <= 0;
+               CurrentCodeWordLen       <= 0;
+               AssembleLenCounter       <= 0;
+               CurrentCodeWord          <= (others => '0');
+               k_meta                   <= (others => '0');
+               SampleCounter            <= 0;
+               SampleCounter_meta       <= (others => '0');
+               ReadyToReciveCodeWordOut <= '1';
 
-               if NewCodeWordIn = '1' then
+               if NewCodeWordReadyIn = '1' then
                   state <= Reciving;
                end if;
 
             when WaitForCodeWord =>
-               CheckState    <= 1;
-               ReadyToRecive <= '1';
+               CheckState               <= 1;
+               ReadyToReciveCodeWordOut <= '1';
 
-               if NewCodeWordIn = '1' then
+               if NewCodeWordReadyIn = '1' then
                   state <= Reciving;
                end if;
 
             when Reciving =>
-               CheckState    <= 2;
-               ReadyToRecive <= '0';
-               
-
+               CheckState               <= 2;
+               ReadyToReciveCodeWordOut <= '0';
                --if it is the first codeword to be assembled the k_value is saved to later be used as meta data
                if TotalLenCounter = 0 then
-                  k_meta <= k_value;
+                  k_meta <= k_valueIn;
                end if;
 
                --Grab the current recived CodeWord
-               CurrentCodeWord <= CodeWord;
+               CurrentCodeWord <= CodeWordIn;
                --Reset assembled bits counter
                AssembleLenCounter <= 0;
                --Grab the length of the current codeword
-               CurrentCodeWordLen <= to_integer(unsigned(CodeWordLength));
+               CurrentCodeWordLen <= to_integer(unsigned(CodeWordLenIn));
                state              <= LengthCheck;
 
                --length check state to see that currentcodeword fits, totallen + currentlen < 6143
@@ -118,7 +115,7 @@ begin
                if AssembleLenCounter < CurrentCodeWordLen then
                   --Assemble the codewords with eachother in "AllCodeWords"
                   --first 13 bits are for metadata
-                  AllCodeWords(8178-TotalLenCounter) <= CurrentCodeWord(1023-AssembleLenCounter);
+                  AllCodeWordsOut(8178 - TotalLenCounter) <= CurrentCodeWord(1023 - AssembleLenCounter);
                   --Increment the assembleLenCOunter by 1
                   AssembleLenCounter <= AssembleLenCounter + 1;
 
@@ -127,7 +124,7 @@ begin
 
                   --once all the bits in the current codeword have been assembled check if all 256 samples have been recived
                   --if the SampleCounter is less than 256 recive another sample
-               elsif SampleCounter < 256 then
+               elsif SampleCounter < 64 then
                   state <= WaitForCodeWord;
 
                   --if all the samples have been assembled set the metadata
@@ -137,7 +134,7 @@ begin
                      state <= ErrorState;
                   else
                      SampleCounter_meta <= std_logic_vector(to_unsigned(SampleCounter - 1, 8));
-                     TotalLenCounter <= TotalLenCounter + 13;
+                     TotalLenCounter    <= TotalLenCounter + 13;--13 bits of meta data
                      state              <= SetMetaData;
                   end if;
                end if;
@@ -146,32 +143,29 @@ begin
                CheckState <= 5;
 
                --Set the k-value meta data
-               AllCodeWords(8191) <= k_meta(4);
-               AllCodeWords(8190) <= k_meta(3);
-               AllCodeWords(8189) <= k_meta(2);
-               AllCodeWords(8188) <= k_meta(1);
-               AllCodeWords(8187) <= k_meta(0);
+               AllCodeWordsOut(8191) <= k_meta(4);
+               AllCodeWordsOut(8190) <= k_meta(3);
+               AllCodeWordsOut(8189) <= k_meta(2);
+               AllCodeWordsOut(8188) <= k_meta(1);
+               AllCodeWordsOut(8187) <= k_meta(0);
 
                --Set the SampleCounter metadata
-               AllCodeWords(8186) <= SampleCounter_meta(7);
-               AllCodeWords(8185) <= SampleCounter_meta(6);
-               AllCodeWords(8184) <= SampleCounter_meta(5);
-               AllCodeWords(8183) <= SampleCounter_meta(4);
-               AllCodeWords(8182) <= SampleCounter_meta(3);
-               AllCodeWords(8181) <= SampleCounter_meta(2);
-               AllCodeWords(8180) <= SampleCounter_meta(1);
-               AllCodeWords(8179) <= SampleCounter_meta(0);
-
-               --Set the total length out
-               AllCodeWordsLength <= std_logic_vector(to_unsigned(TotalLenCounter, 13));
-
-               state <= Sending;
+               AllCodeWordsOut(8186) <= SampleCounter_meta(7);
+               AllCodeWordsOut(8185) <= SampleCounter_meta(6);
+               AllCodeWordsOut(8184) <= SampleCounter_meta(5);
+               AllCodeWordsOut(8183) <= SampleCounter_meta(4);
+               AllCodeWordsOut(8182) <= SampleCounter_meta(3);
+               AllCodeWordsOut(8181) <= SampleCounter_meta(2);
+               AllCodeWordsOut(8180) <= SampleCounter_meta(1);
+               AllCodeWordsOut(8179) <= SampleCounter_meta(0);
+               state                 <= Sending;
 
             when Sending =>
-               CheckState   <= 6;
-               AssembleDone <= '1';
-
-               state <= Idle;
+               CheckState      <= 6;
+               AssembleDoneOut <= '1';
+               if DataSavedIn = '1' then
+                  state <= Idle;
+               end if;
 
             when ErrorState =>
                CheckState <= 10;
@@ -179,19 +173,18 @@ begin
          end case;
 
          if reset = '1' then
-            state              <= Idle;
-            AllCodeWords       <= (others => '0');
-            ErrorOut           <= '0';
-            AssembleDone       <= '0';
-            TotalLenCounter    <= 0;
-            CurrentCodeWordLen <= 0;
-            AssembleLenCounter <= 0;
-            CurrentCodeWord    <= (others => '0');
-            SampleCounter      <= 0;
-            SampleCounter_meta <= (others => '0');
-            k_meta             <= (others => '0');
-            AllCodeWordsLength <= (others => '0');
-            ReadyToRecive      <= '0';
+            state                    <= Idle;
+            AllCodeWordsOut          <= (others => '0');
+            ErrorOut                 <= '0';
+            AssembleDoneOut          <= '0';
+            TotalLenCounter          <= 0;
+            CurrentCodeWordLen       <= 0;
+            AssembleLenCounter       <= 0;
+            CurrentCodeWord          <= (others => '0');
+            SampleCounter            <= 0;
+            SampleCounter_meta       <= (others => '0');
+            k_meta                   <= (others => '0');
+            ReadyToReciveCodeWordOut <= '0';
 
          end if;
 
