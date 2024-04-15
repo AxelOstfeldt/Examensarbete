fe66837289858c0786fc42a6e0937c7d9e6b4743
AxelOstfeldt <115161796+AxelOstfeldt@users.noreply.github.com>
2024-04-15 12:47:05 +0200

    Updated files to work with full system

--- VHDL/src/NewRiceEncode.vhd
+++ VHDL/src/NewRiceEncode.vhd
@@ -7,42 +7,48 @@ use ieee.numeric_std.all;
 entity NewRiceEncoder is
 
    port (
-      reset         : in std_logic;
-      clk           : in std_logic;
-      OriginalValue : in std_logic_vector(23 downto 0);--value in to be encoded
-      k_value       : in std_logic_vector(4 downto 0);
-      NewDataIn     : in std_logic;--New data available for input
+      reset                   : in std_logic;
+      clk                     : in std_logic;
+      AllResidualsIn          : in std_logic_vector(1599 downto 0);--value in to be encoded
+      kValueIn                : in std_logic_vector(4 downto 0);
+      NewKIn                  : in std_logic;--New data available for input
+      NewResidualsIn          : in std_logic;
+      ReadytoReciveCodeWordIn : in std_logic;
 
-      NewDataOut     : out std_logic;--New CodeWord ready to be sent
-      ErrorOut       : out std_logic;--Send an error signal when error state is reached
-      CodeWordLength : out std_logic_vector(9 downto 0);--Bits used in the codeword
-      CodeWord       : out std_logic_vector(1023 downto 0)--CodeWord out
-      
+      NewCodeWordReady : out std_logic;--New CodeWord ready to be sent
+      ReadyToEncode    : out std_logic;
+      ErrorOut         : out std_logic;--Send an error signal when error state is reached
+      CodeWordLen      : out std_logic_vector(9 downto 0);--Bits used in the codeword
+      CodeWord         : out std_logic_vector(1023 downto 0)--CodeWord out
+
    );
 
 end entity;
 
 architecture Behavioral of NewRiceEncoder is
-   type state_type is (Idle, Reciving, SignBit, FindKLastBits, FindRleCode, RleCoding, SetKLastBits, Sending, ErrorState); -- States for the statemachine
+   type state_type is (Idle, Reciving, NewResidual, SetValue, SignBit, FindKLastBits, FindRleCode, RleCoding, SetKLastBits, Sending, ErrorState); -- States for the statemachine
    signal state : state_type;
    --used in all states
-   signal LenCounter      : integer range 0 to 1023;--count length of code word
-   signal CurrentCodeWord : std_logic_vector(1023 downto 0);--set bits for current codeword
-   signal CurrentValue    : signed(23 downto 0);--current value being encoded
-   signal k_val_int       : integer range 0 to 32;
-   signal k_pow           : integer range 0 to 1073741824;-- max val is 30^2, max val of k^2
-   signal abs_bits_set    : integer range 0 to 32;--counts how many of the last bits are set
-   signal CheckState      : integer range 0 to 10;--to see what state is currently used for the simulation
-
+   signal LenCounter          : integer range 0 to 1023;--count length of code word
+   signal CurrentCodeWord     : std_logic_vector(1023 downto 0);--set bits for current codeword
+   signal AllCurrentResiduals : std_logic_vector(1599 downto 0);
+   signal CurrentResidual     : std_logic_vector(24 downto 0);
+   signal CurrentValue        : signed(24 downto 0);--current value being encoded
+   signal k_val_int           : integer range 0 to 32;
+   signal k_pow               : integer range 0 to 1073741824;-- max val is 30^2, max val of k^2
+   signal abs_bits_set        : integer range 0 to 32;--counts how many of the last bits are set
+   signal CheckState          : integer range 0 to 10;--to see what state is currently used for the simulation
+   signal ResidualBitCounter  : integer range 0 to 1600;
+   signal SampleCounter       : integer range 0 to 64;
    --used in SignBit state
-   signal AbsoluteValue : std_logic_vector(23 downto 0);
+   signal AbsoluteValue : std_logic_vector(24 downto 0);
 
    --Used in **KLastBits
-   signal kLastBits : std_logic_vector(23 downto 0);
+   signal kLastBits : std_logic_vector(24 downto 0);
    signal kBitsSet  : integer range 0 to 32;
 
    --Used in **RleCoding
-   signal RleBits : std_logic_vector(23 downto 0);
+   signal RleBits : std_logic_vector(24 downto 0);
    signal RleLoop : integer range 0 to 1023;
 
 begin
@@ -56,38 +62,112 @@ begin
                CheckState <= 0;
 
                -- default values for signals
-               k_val_int       <= to_integer(unsigned(k_value));
-               k_pow           <= 2 ** to_integer(unsigned(k_value));
-               LenCounter      <= 0;
-               CurrentCodeWord <= (others => '0');
-               CurrentValue    <= (others => '0');
-               NewDataOut      <= '0';
-               AbsoluteValue   <= (others => '0');
-               kLastBits       <= (others => '0');
-               RleBits         <= (others => '0');
-               RleLoop         <= 0;
-               abs_bits_set    <= 0;
-               kBitsSet        <= 0;
-               ErrorOut        <= '0';
+               k_val_int           <= to_integer(unsigned(kValueIn));
+               k_pow               <= 2 ** to_integer(unsigned(kValueIn));
+               LenCounter          <= 0;
+               CurrentCodeWord     <= (others => '0');
+               CurrentValue        <= (others => '0');
+               NewCodeWordReady    <= '0';
+               AbsoluteValue       <= (others => '0');
+               kLastBits           <= (others => '0');
+               RleBits             <= (others => '0');
+               RleLoop             <= 0;
+               abs_bits_set        <= 0;
+               kBitsSet            <= 0;
+               ErrorOut            <= '0';
+               CurrentResidual     <= (others => '0');
+               AllCurrentResiduals <= (others => '0');
+               ResidualBitCounter  <= 0;
+               SampleCounter       <= 0;
+               ReadyToEncode       <= '1';
 
                -- when new data is available go to reciving state
-               if NewDataIn = '1' then
+               if (NewKIn = '1') and (NewResidualsIn = '1') then
                   state <= Reciving;
                end if;
 
             when Reciving =>
                --Indicate what the current state is in simulation
                CheckState <= 1;
+               ReadyToEncode <= '0';
 
-               -- load the new Codeword
-               CurrentValue <= signed(OriginalValue);
+               -- load all residuals
+               AllCurrentResiduals <= AllResidualsIn;
 
-               --when data is loaded go to SignBit state
+               state <= NewResidual;
+
+            when NewResidual =>
+               CheckState <= 2;
+               ReadyToEncode <= '0';
+
+               CurrentValue     <= (others => '0');
+               NewCodeWordReady <= '0';
+               AbsoluteValue    <= (others => '0');
+               kLastBits        <= (others => '0');
+               RleBits          <= (others => '0');
+               RleLoop          <= 0;
+               abs_bits_set     <= 0;
+               kBitsSet         <= 0;
+               --Set the current residual from the datablock with all residuals
+               CurrentResidual(24) <= AllCurrentResiduals(1599 - ResidualBitCounter);
+               CurrentResidual(23) <= AllCurrentResiduals(1598 - ResidualBitCounter);
+               CurrentResidual(22) <= AllCurrentResiduals(1597 - ResidualBitCounter);
+               CurrentResidual(21) <= AllCurrentResiduals(1596 - ResidualBitCounter);
+               CurrentResidual(20) <= AllCurrentResiduals(1595 - ResidualBitCounter);
+               CurrentResidual(19) <= AllCurrentResiduals(1594 - ResidualBitCounter);
+               CurrentResidual(18) <= AllCurrentResiduals(1593 - ResidualBitCounter);
+               CurrentResidual(17) <= AllCurrentResiduals(1592 - ResidualBitCounter);
+               CurrentResidual(16) <= AllCurrentResiduals(1591 - ResidualBitCounter);
+               CurrentResidual(15) <= AllCurrentResiduals(1590 - ResidualBitCounter);
+               CurrentResidual(14) <= AllCurrentResiduals(1589 - ResidualBitCounter);
+               CurrentResidual(13) <= AllCurrentResiduals(1588 - ResidualBitCounter);
+               CurrentResidual(12) <= AllCurrentResiduals(1587 - ResidualBitCounter);
+               CurrentResidual(11) <= AllCurrentResiduals(1586 - ResidualBitCounter);
+               CurrentResidual(10) <= AllCurrentResiduals(1585 - ResidualBitCounter);
+               CurrentResidual(9)  <= AllCurrentResiduals(1584 - ResidualBitCounter);
+               CurrentResidual(8)  <= AllCurrentResiduals(1583 - ResidualBitCounter);
+               CurrentResidual(7)  <= AllCurrentResiduals(1582 - ResidualBitCounter);
+               CurrentResidual(6)  <= AllCurrentResiduals(1581 - ResidualBitCounter);
+               CurrentResidual(5)  <= AllCurrentResiduals(1580 - ResidualBitCounter);
+               CurrentResidual(4)  <= AllCurrentResiduals(1579 - ResidualBitCounter);
+               CurrentResidual(3)  <= AllCurrentResiduals(1578 - ResidualBitCounter);
+               CurrentResidual(2)  <= AllCurrentResiduals(1577 - ResidualBitCounter);
+               CurrentResidual(1)  <= AllCurrentResiduals(1576 - ResidualBitCounter);
+               CurrentResidual(0)  <= AllCurrentResiduals(1575 - ResidualBitCounter);
+               -- Residual bits can never be larger than 1575, since that gives a negative argument to AllCurrentResiduals
+               if ResidualBitCounter < 1551 then
+                  --increment ResidualBitCounter so that the next residuals will be grabbed from allresiduals when this state is called next time
+                  ResidualBitCounter <= ResidualBitCounter + 25;
+
+               else
+                  state <= ErrorState;
+
+               end if;
+
+               --for adjacent each datablock should have 64 residuals, one for every mic in the array
+               if SampleCounter < 64 then
+                  --increment the sample counter since 1 more residual have been grabbed from all residuals
+                  SampleCounter <= SampleCounter + 1;
+
+               else
+                  state <= ErrorState;
+
+               end if;
+               --when the current residual is grabbed set the value to encode to the current residual
+               state <= SetValue;
+
+            when SetValue =>
+               CheckState <= 3;
+               ReadyToEncode <= '0';
+
+               CurrentValue <= signed(CurrentResidual);
+
                state <= SignBit;
 
             when SignBit =>
                --Indicate what the current state is in simulation
-               CheckState <= 2;
+               CheckState <= 4;
+               ReadyToEncode <= '0';
 
                --Check if CurrentValue is positive or negative, 
                --this decides the sign bit (MSB) for the codeword
@@ -112,7 +192,8 @@ begin
 
             when FindKLastBits =>
                --Indicate what the current state is in simulation
-               CheckState <= 3;
+               CheckState <= 5;
+               ReadyToEncode <= '0';
 
                --set bits so that the k-last bits are saved to later be used in the codeword
                if abs_bits_set < k_val_int then
@@ -128,10 +209,11 @@ begin
 
             when FindRleCode =>
                --Indicate what the current state is in simulation
-               CheckState <= 4;
+               CheckState <= 6;
+               ReadyToEncode <= '0';
 
                --set the remaining bits in Absolute value to the LSB:s in RleBits to later Run length encode the resulting value
-               if abs_bits_set < 24 then
+               if abs_bits_set < 25 then
                   RleBits(abs_bits_set - k_val_int) <= AbsoluteValue(abs_bits_set);
 
                   abs_bits_set <= abs_bits_set + 1;
@@ -151,7 +233,8 @@ begin
 
             when RleCoding =>
                --Indicate what the current state is in simulation
-               CheckState <= 5;
+               CheckState <= 7;
+               ReadyToEncode <= '0';
 
                --If RleLoop is larger than 0, append a 1 to the codeword and decrement RleLoop value
                if RleLoop > 0 then
@@ -183,9 +266,11 @@ begin
                   state <= SetKLastBits;
 
                end if;
+
             when SetKLastBits =>
                --Indicate what the current state is in simulation
-               CheckState <= 6;
+               CheckState <= 8;
+               ReadyToEncode <= '0';
 
                --set the k-last bits of the code word
                if kBitsSet < k_val_int then
@@ -207,8 +292,8 @@ begin
                else
                   --The codeword is now finalised,
                   --set the output variables for codeword and codeword length
-                  CodeWord       <= CurrentCodeWord;
-                  CodeWordLength <= std_logic_vector(to_unsigned(LenCounter, 10));
+                  CodeWord    <= CurrentCodeWord;
+                  CodeWordLen <= std_logic_vector(to_unsigned(LenCounter, 10));
 
                   state <= Sending;
 
@@ -216,12 +301,24 @@ begin
 
             when Sending =>
                --Indicate what the current state is in simulation
-               CheckState <= 7;
+               CheckState <= 9;
+               ReadyToEncode <= '0';
 
                --set NewDataOut to 1 to indicate that new data is ready to be sent
-               NewDataOut <= '1';
+               NewCodeWordReady <= '1';
 
-               state <= Idle;
+               --wait until CodeWordAssembler is ready to recive the codeword beffore going to the next state
+               if ReadytoReciveCodeWordIn = '1' then
+                  --If samplecounter = 64 all residuals have been looked at in the datablock, and it is ready to recive a new datablock
+                  if SampleCounter = 64 then
+                     state <= Idle;
+
+                     --Else the next residual in the current datablock will be grabbed
+                  else
+                     state <= NewResidual;
+
+                  end if;
+               end if;
 
             when ErrorState =>
                --added State to handle errors
@@ -229,24 +326,31 @@ begin
                ErrorOut   <= '1';
          end case;
          if reset = '1' then
-            if to_integer(unsigned(k_value)) < 8 then
+
+            if (to_integer(unsigned(kValueIn)) < 8) or (to_integer(unsigned(kValueIn)) > 22) then
                --k_value have to be atleast 8,
                --to limit max lenght of code words in niche cases
                state <= ErrorState;
 
             else
                state     <= Idle;
-               k_val_int <= to_integer(unsigned(k_value));
-               k_pow     <= 2 ** to_integer(unsigned(k_value));
+               k_val_int <= to_integer(unsigned(kValueIn));
+               k_pow     <= 2 ** to_integer(unsigned(kValueIn));
 
             end if;
-            ErrorOut        <= '0';
-            LenCounter      <= 0;
-            CurrentCodeWord <= (others => '0');
-            CurrentValue    <= (others => '0');
-            NewDataOut      <= '0';
-            abs_bits_set    <= 0;
-            kBitsSet        <= 0;
+
+            ErrorOut            <= '0';
+            LenCounter          <= 0;
+            CurrentCodeWord     <= (others => '0');
+            CurrentValue        <= (others => '0');
+            NewCodeWordReady    <= '0';
+            abs_bits_set        <= 0;
+            kBitsSet            <= 0;
+            CurrentResidual     <= (others => '0');
+            AllCurrentResiduals <= (others => '0');
+            ResidualBitCounter  <= 0;
+            SampleCounter       <= 0;
+            ReadyToEncode       <= '0';
 
          end if;
 
--- VHDL/src/kCalculator.vhd
+++ VHDL/src/kCalculator.vhd
@@ -11,6 +11,7 @@ entity kCalculator is
       clk             : in std_logic;
       ResidualValueIn : in std_logic_vector(24 downto 0);--value in to be encoded
       NewDataIn       : in std_logic;--New data available for input
+      ReadyToEncode   : in std_logic;
 
       ReadyToReciveOut : out std_logic;
       NewKOut          : out std_logic;--New k-value to be sent
@@ -27,7 +28,7 @@ architecture Behavioral of kCalculator is
 
    signal CheckState    : integer range 0 to 10;--to see what state is currently used for the simulation
    signal CurrentValue  : signed(24 downto 0);
-   signal SampleCounter : integer range 0 to 256;--count how many samples have been recived, assuming 256 samples per datablock
+   signal SampleCounter : integer range 0 to 256;--count how many samples have been recived, assuming maximum 256 samples per datablock
    signal TotalSum      : integer range 0 to 1300000000;
 begin
    process (clk)
@@ -38,12 +39,12 @@ begin
             when Idle =>
                CheckState <= 0;
 
-               SampleCounter <= 0;
-               TotalSum      <= 0;
+               SampleCounter    <= 0;
+               TotalSum         <= 0;
                ReadyToReciveOut <= '1';
-               NewKOut       <= '0';
-               ErrorOut      <= '0';
-               CurrentValue  <= (others => '0');
+               NewKOut          <= '0';
+               ErrorOut         <= '0';
+               CurrentValue     <= (others => '0');
 
                if NewDataIn = '1' then
                   state <= Reciving;
@@ -53,8 +54,8 @@ begin
                CheckState <= 1;
 
                ReadyToReciveOut <= '0';
-               CurrentValue  <= signed(ResidualValueIn);
-               SampleCounter <= SampleCounter + 1;
+               CurrentValue     <= signed(ResidualValueIn);
+               SampleCounter    <= SampleCounter + 1;
 
                state <= SumValues;
 
@@ -64,12 +65,16 @@ begin
                TotalSum <= TotalSum + to_integer(abs(CurrentValue));
 
                --if TotalSum is equal or larger than 1261097232 the best k-value is allway 22 (for all inputs of 24 bit signed)
-               if TotalSum >= 1261097232 then
+               --This limit is set after "4 926 122*256", since if that value is reached for totalsum k will allways be 22.
+               --This assumes that 256 samples will be used, however when looking at just one microphone array only 64 samples per datablock is used
+               --The limit could therefore be lowered. 
+               if TotalSum >= 1261087232 then
                   state <= CalculateK;
 
-                  --if 256 samples have been looked at the full datablock have been sumarised and it is time to find the ideal k-value,
+                  --if 64 samples (assuming 64 samples per datablock, if more samples per data block is used this number needs to adapt after it)
+                  --have been looked at the full datablock have been sumarised and it is time to find the ideal k-value,
                   -- else another sample is grabbed
-               elsif Samplecounter < 256 then
+               elsif Samplecounter < 64 then
                   state <= WaitForValue;
 
                else
@@ -78,7 +83,7 @@ begin
                end if;
 
             when WaitForValue =>
-               CheckState    <= 3;
+               CheckState       <= 3;
                ReadyToReciveOut <= '1';
 
                if NewDataIn = '1' then
@@ -90,50 +95,52 @@ begin
 
                --if statement for differente TotalSum levels to find best k-values
                --these levels have been derive from:
-               -- k = log2(log10(2) * x /256) + 1, where x is the TotalSum
-               --at every if statement the x value for the given limit is written * 256
-               --this is so that it will be easy to calculate new limits if the sample size per block where to change
+               -- k = log2(log10(2) * x /s ) + 1, where "x" is the TotalSum and "s" is the total amount of samples
+               -- to avoid devision the limits for the total sum have been pre calculated
+               -- the limits assume 64 datasamples per datablock, this is one microphone array.
+               -- but the limits for all microphones, 256 samples per datablock have also been calculated and can be found in the comment
+               -- along with how the calculations was made so that the limit can easily be adjusted in the future by multiplying with a new amount of samples
 
-               if TotalSum < 154112 then--602*256
+               if TotalSum < 38528 then--602*64 = 38 528, 602*256 = 154112
                   kValueOut <= "01000";--8
 
-               elsif TotalSum < 307968 then--1203*256
+               elsif TotalSum <  76992 then--1203 * 64 = 76 992, 1203*256 = 307968
                   kValueOut <= "01001";--9
 
-               elsif TotalSum < 615936 then--2406*256
+               elsif TotalSum < 153984 then--2406 * 64 = 153 984, 2406*256 = 615936
                   kValueOut <= "01010";--10
 
-               elsif TotalSum < 1231616 then--4811*256
+               elsif TotalSum < 307904 then--4811 * 64 = 307 904, 4811*256 = 1231616
                   kValueOut <= "01011";--11
 
-               elsif TotalSum < 2463232 then--9622*256
+               elsif TotalSum < 615808 then--9622 * 64 = 615 808, 9622*256 = 2463232
                   kValueOut <= "01100";--12
 
-               elsif TotalSum < 4926208 then--19243*256
+               elsif TotalSum < 1231552 then--19234 * 64 = 1 231 552, 19243*256 = 4926208
                   kValueOut <= "01101";--13
 
-               elsif TotalSum < 9852416 then--38486*256
+               elsif TotalSum < 2463104 then--38486 * 64 = 2 463 104,38486*256 = 9852416
                   kValueOut <= "01110";--14
 
-               elsif TotalSum < 19704576 then--76971*256
+               elsif TotalSum < 4926144 then--76971 * 64 = 4 926 144, 76971*256 = 19704576
                   kValueOut <= "01111";--15
 
-               elsif TotalSum < 39409152 then--153942*256
+               elsif TotalSum < 9852288 then--153942 * 64 = 9 852 288, 153942*256 = 39409152
                   kValueOut <= "10000";--16
 
-               elsif TotalSum < 78818048 then--307883*256
+               elsif TotalSum < 19704512 then--307 883 * 64 = 19 704 512, 307883*256 = 78818048
                   kValueOut <= "10001";--17
 
-               elsif TotalSum < 157636096 then--615766*256
+               elsif TotalSum < 39409024 then--615 766 * 64 = 39 409 024, 615766*256 = 157636096
                   kValueOut <= "10010";--18
 
-               elsif TotalSum < 315271936 then--1231531*256
+               elsif TotalSum < 78817984 then--1 231 531 * 64 = 78 817 984, 1231531*256 = 315271936
                   kValueOut <= "10011";--19
 
-               elsif TotalSum < 630543616 then--2463061*256
+               elsif TotalSum < 157635904 then--2 463 061 * 64 = 157 635 904, 2463061*256 = 630543616
                   kValueOut <= "10100";--20
 
-               elsif TotalSum < 1261087232 then--4926122*256
+               elsif TotalSum < 315271808 then--4 926 122 * 64 = 315 271 808, 4926122*256 = 1261087232
                   kValueOut <= "10101";--21
 
                else
@@ -146,9 +153,14 @@ begin
             when Sending =>
                CheckState <= 5;
 
+               --New k value calculated, ready to be sent out
                NewKOut <= '1';
 
-               state <= Idle;
+               --Once ready to encode is =1 RiceEncoder block is ready to recive the new k-value
+               --When it have been sent return to idle state to wait for the next residuals to calculate new k-value
+               if ReadyToEncode = '1' then
+                  state <= Idle;
+               end if;
 
             when ErrorState =>
                CheckState <= 10;
@@ -157,15 +169,15 @@ begin
          end case;
 
          if reset = '1' then
-            state         <= Idle;
-            SampleCounter <= 0;
-            TotalSum      <= 0;
+            state            <= Idle;
+            SampleCounter    <= 0;
+            TotalSum         <= 0;
             ReadyToReciveOut <= '0';
-            NewKOut       <= '0';
-            ErrorOut      <= '0';
-            kValueOut       <= (others => '0');
-            kValueOut       <= "01000";
-            CurrentValue  <= (others => '0');
+            NewKOut          <= '0';
+            ErrorOut         <= '0';
+            kValueOut        <= (others => '0');
+            kValueOut        <= "01000";
+            CurrentValue     <= (others => '0');
 
          end if;
 
