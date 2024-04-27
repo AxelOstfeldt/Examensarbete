35e51364c858ba5e863e7539d385141af917936b
axos8381 <axel.ostfeldt.8381@student.uu.se>
2024-04-27 12:43:25 +0200

    ???

---
+++ VHDL/run.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from vunit import VUnit
+
+
+# NOTE: Path to directory containing this file
+ROOT = Path(__file__).parent
+
+vu = VUnit.from_argv(compile_builtins=False)  # Stop using the builtins ahead of time.vu.add_vhdl_builtins() #new for version 5 VUnit
+vu.add_vhdl_builtins() #new for version 5 VUnit
+
+lib = vu.add_library("lib")
+lib.add_source_files(ROOT.glob("test/tb_adjacent_combined.vhd"))
+
+
+lib.add_source_files(ROOT.glob("src/**/*.vhd"))
+
+
+for l in lib.get_test_benches():
+   wave = ROOT.joinpath("test", "wave", f"{l.name}.tcl")
+   l.set_sim_option("ghdl.gtkwave_script.gui", str(wave) if wave.is_file() else str(ROOT / "gtkwave.tcl"))
+
+vu.main()
